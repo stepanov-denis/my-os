bc8dc19598a8a736a059d0573eef4b08a966e1bb
stepanov-denis <stepanov.d.m@yandex.ru>
2022-02-24 14:50:37 +0700

    Add library

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,10 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(my_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use my_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -19,5 +20,10 @@ fn test_runner(test: &[&dyn Fn()]) {
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}
+    my_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
